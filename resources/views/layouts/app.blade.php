fe168e62762d22fb757059576d1472bdfa8c2bb7
danielvilla332 <63080405+danielvilla332@users.noreply.github.com>
2023-11-24 21:04:52 -0500

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+@extends('adminlte::page')
+<link rel="icon" href="vendor/adminlte/dist/img/TecnimotosLogo.png"> 
+@section('title', 'Tecnimotos')
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
